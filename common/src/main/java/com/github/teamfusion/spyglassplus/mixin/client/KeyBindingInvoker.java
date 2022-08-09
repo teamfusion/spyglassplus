27f8da13498a9164ef61a43534767c3d365c373d
andantet <orrdaniel73@gmail.com>
2022-08-09 12:54:29 +0100

    various protections for input while scoping

---
+++ common/src/main/java/com/github/teamfusion/spyglassplus/mixin/client/KeyBindingInvoker.java
@@ -0,0 +1,13 @@
+package com.github.teamfusion.spyglassplus.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.option.KeyBinding;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Environment(EnvType.CLIENT)
+@Mixin(KeyBinding.class)
+public interface KeyBindingInvoker {
+    @Invoker void invokeReset();
+}
---
+++ common/src/main/java/com/github/teamfusion/spyglassplus/mixin/client/MinecraftClientMixin.java
@@ -0,0 +1,68 @@
+package com.github.teamfusion.spyglassplus.mixin.client;
+
+import com.github.teamfusion.spyglassplus.entity.ScopingPlayer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.GameOptions;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Environment(EnvType.CLIENT)
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Shadow @Nullable public ClientPlayerEntity player;
+    @Shadow @Final public GameOptions options;
+
+    /**
+     * Cancels attacks when scoping.
+     */
+    @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
+    private void onDoAttack(CallbackInfoReturnable<Boolean> cir) {
+        ScopingPlayer scopingPlayer = ScopingPlayer.cast(this.player);
+        if (scopingPlayer.isScoping()) cir.setReturnValue(false);
+    }
+
+    /**
+     * Cancels block breaking when scoping.
+     */
+    @ModifyArg(
+        method = "handleInputEvents",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/MinecraftClient;handleBlockBreaking(Z)V"
+        ),
+        index = 0
+    )
+    private boolean onHandleBlockBreaking(boolean input) {
+        ScopingPlayer scopingPlayer = ScopingPlayer.cast(this.player);
+        if (scopingPlayer.isScoping()) return false;
+        return input;
+    }
+
+    /**
+     * Cancels item usage when scoping.
+     */
+    @Inject(method = "doItemUse", at = @At("HEAD"), cancellable = true)
+    private void onDoItemUse(CallbackInfo ci) {
+        ScopingPlayer scopingPlayer = ScopingPlayer.cast(this.player);
+        if (scopingPlayer.isScoping()) ci.cancel();
+    }
+
+    /**
+     * Cancels hotbar key binds when scoping.
+     */
+    @Inject(method = "handleInputEvents", at = @At("HEAD"))
+    private void onHandleInputEvents(CallbackInfo ci) {
+        ScopingPlayer scopingPlayer = ScopingPlayer.cast(this.player);
+        if (scopingPlayer.isScoping()) for (int i = 0; i < 9; ++i) ((KeyBindingInvoker) this.options.hotbarKeys[i]).invokeReset();
+    }
+}
