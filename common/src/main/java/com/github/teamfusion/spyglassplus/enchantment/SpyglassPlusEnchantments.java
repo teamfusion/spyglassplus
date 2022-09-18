cef7612cb1cb7c4441ce473e730d3b83cbceac35
andantet <orrdaniel73@gmail.com>
2022-09-18 12:09:42 +0100

    overcomplicate SpyglassPlusEnchantments for no reason

--- common/src/main/java/com/github/teamfusion/spyglassplus/enchantment/SpyglassPlusEnchantments.java
+++ common/src/main/java/com/github/teamfusion/spyglassplus/enchantment/SpyglassPlusEnchantments.java
@@ -6,19 +6,22 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.registry.Registry;
 
+import java.util.function.Function;
+
 import static net.minecraft.enchantment.Enchantment.Rarity.COMMON;
 import static net.minecraft.enchantment.Enchantment.Rarity.UNCOMMON;
 
 public interface SpyglassPlusEnchantments {
     DeferredRegister<Enchantment> REGISTER = DeferredRegister.create(SpyglassPlus.MOD_ID, Registry.ENCHANTMENT_KEY);
 
-    RegistrySupplier<Enchantment> SCRUTINY = register("scrutiny", new ScrutinyEnchantment(UNCOMMON));
-    RegistrySupplier<Enchantment> ILLUMINATE = register("illuminate", new IlluminateEnchantment(UNCOMMON));
-    RegistrySupplier<Enchantment> INDICATE = register("indicate", new IndicateEnchantment(UNCOMMON));
-    RegistrySupplier<Enchantment> DISCOVERY = register("discovery", new DiscoveryEnchantment(UNCOMMON));
-    RegistrySupplier<Enchantment> COMMAND = register("command", new CommandEnchantment(COMMON));
+    RegistrySupplier<Enchantment> SCRUTINY = register("scrutiny", ScrutinyEnchantment::new, UNCOMMON);
+    RegistrySupplier<Enchantment> ILLUMINATE = register("illuminate", IlluminateEnchantment::new, UNCOMMON);
+    RegistrySupplier<Enchantment> INDICATE = register("indicate", IndicateEnchantment::new, UNCOMMON);
+    RegistrySupplier<Enchantment> DISCOVERY = register("discovery", DiscoveryEnchantment::new, UNCOMMON);
+    RegistrySupplier<Enchantment> COMMAND = register("command", CommandEnchantment::new, COMMON);
 
-    private static RegistrySupplier<Enchantment> register(String id, Enchantment enchantment) {
+    private static RegistrySupplier<Enchantment> register(String id, Function<Enchantment.Rarity, Enchantment> factory, Enchantment.Rarity rarity) {
+        Enchantment enchantment = factory.apply(rarity);
         return REGISTER.register(id, () -> enchantment);
     }
 }
