366277b0a3659a56c21a62da606b5e2c973cdc32
andantet <orrdaniel73@gmail.com>
2022-09-19 16:45:07 +0100

    mark SpyglassPlusKeyBindings as client-only

--- common/src/main/java/com/github/teamfusion/spyglassplus/client/keybinding/SpyglassPlusKeyBindings.java
+++ common/src/main/java/com/github/teamfusion/spyglassplus/client/keybinding/SpyglassPlusKeyBindings.java
@@ -2,9 +2,12 @@ package com.github.teamfusion.spyglassplus.client.keybinding;
 
 import com.github.teamfusion.spyglassplus.SpyglassPlus;
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
+@Environment(EnvType.CLIENT)
 public interface SpyglassPlusKeyBindings {
     String CATEGORY = "key.category." + SpyglassPlus.MOD_ID;
 
